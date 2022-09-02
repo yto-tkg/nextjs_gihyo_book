cb7318a030fd720f71adad00480f58fcac0bc88f
yto-tkg <yuto-takagi@cybozu.co.jp>
2022-09-02 18:19:38 +0900

    Stroybookの設定

---
+++ src/fontSizes.ts
@@ -0,0 +1,18 @@
+const fontSizes: any = {
+  extraSmall: '12px',
+  small: '14px',
+  medium: '16px',
+  mediumLarge: '20px',
+  large: '24px',
+  extraLarge: '32px',
+}
+
+// aliases
+fontSizes.extraSmall = fontSizes[0]
+fontSizes.small = fontSizes[1]
+fontSizes.medium = fontSizes[2]
+fontSizes.mediumLarge = fontSizes[3]
+fontSizes.large = fontSizes[4]
+fontSizes.extraLarge = fontSizes[5]
+
+export default fontSizes
